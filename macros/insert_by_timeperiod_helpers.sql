410ad97fd194bf3353fa67077be2b10b77b2b34e
Tom O'Neill <aslittlesliceoftom@notavalidemail.com>
2024-09-09 17:27:29 +0200

    remove nested curlies

--- macros/insert_by_timeperiod_helpers.sql
+++ macros/insert_by_timeperiod_helpers.sql
@@ -78,7 +78,7 @@
 
     """ Generic macro to get the period filter to date. Note this provides text that can be used in a SQL statement, rather than a computed date."""
     {%- set period_filter_from -%}
-        CAST({{ dateadd(period, offset, '{{start_timestamp}}') }} AS DATE)
+        CAST({{ dateadd(period, offset, start_timestamp) }} AS DATE)
     {%- endset -%}
     {% do return(period_filter_from) %}
 {% endmacro %}
@@ -86,7 +86,7 @@
 {%- macro get_period_filter_to(period, offset,start_timestamp, stop_timestamp)%}
 """ Generic macro to get the period filter to date. Note this provides text that can be used in a SQL statement, rather than a computed date."""
     {%- set period_filter_to -%}
-        LEAST({{dateadd(period, offset + 1, '{{start_timestamp}}') }}, '{{ stop_timestamp if stop_timestamp is not none else "9999-12-31" }}')
+        LEAST({{dateadd(period, offset + 1, start_timestamp) }}, '{{ stop_timestamp if stop_timestamp is not none else "9999-12-31" }}')
     {%- endset -%}
     {% do return(period_filter_to) %}
 {% endmacro %}
